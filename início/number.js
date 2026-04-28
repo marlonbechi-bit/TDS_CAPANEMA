d44bda3ba09993068b20d19c10c9b087868376de
marlon.bechi-bit <marlon.bechi@gmail.com>
2026-04-27 21:48:00 -0300

    Fundamentos de JavaScript

---
+++ início/number.js
@@ -0,0 +1,9 @@
+let idade = 24;
+let altura = 1.76;
+
+console.log(idade);
+console.log(altura);
+
+console.log(idade + altura); // Somar
+console.log(idade, altura); // Concatenar
+
---
+++ início/variable.js
@@ -0,0 +1,10 @@
+var nome = "Marlon";
+let media = 7.0;
+
+console.log (nome);
+
+nome = "Rogério";
+console.log (nome);
+
+media = 7.5;
+console.log(media);
